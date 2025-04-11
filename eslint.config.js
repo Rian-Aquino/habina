f74a3bf0a7ee2907eec58f40e94cc23bf7e70ef0
Rian Aquino <63216809+Rian-Aquino@users.noreply.github.com>
2025-04-10 22:45:11 -0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import pluginImport from 'eslint-plugin-import';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import pluginReact from 'eslint-plugin-react';
+import pluginHooks from 'eslint-plugin-react-hooks';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  pluginJsxA11y.flatConfigs.recommended,
+  pluginHooks.configs['recommended-latest'],
+  pluginImport.flatConfigs.recommended,
+  eslintConfigPrettier,
+
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+      },
+    },
+    rules: {
+      'react/function-component-definition': [
+        'error',
+        {
+          namedComponents: ['function-expression', 'arrow-function'],
+          unnamedComponents: ['function-expression', 'arrow-function'],
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+    },
+  },
+
+  {
+    plugins: {
+      'simple-import-sort': pluginSimpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'warn',
+      'simple-import-sort/exports': 'warn',
+      'import/no-default-export': 'error',
+    },
+  },
+  {
+    files: ['*.config.*'],
+    rules: {
+      'import/no-default-export': 'off',
+    },
+  },
+]);
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export const App = function () {
+  return (
+    <div className="h-full flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Hello, World!</h1>
+    </div>
+  );
+};
